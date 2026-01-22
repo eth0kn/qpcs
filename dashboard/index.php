c4018edf18e302d34dfb179cc4b6b46ff59cca0e
eth0kn <kkandiaskanzah@gmail.com>
2026-01-22 14:17:13 +0700

    final prod

--- dashboard/index.php
+++ dashboard/index.php
@@ -213,7 +213,7 @@
 </div>
 
 <script>
-    const API_BASE_URL = "http://localost:8000"; 
+    const API_BASE_URL = "http://localhost:8000"; 
 
     // --- TABS LOGIC ---
     function switchTab(tab) {
